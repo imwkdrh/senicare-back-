cd645cf152ff6815cf06b413527d68119beef0cf
imwkdrh <imwkdrh7@naver.com>
2024-09-11 10:51:54 +0900

    2024-09-11 10:51

---
+++ src/main/java/com/korit/senicare/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.korit.senicare.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.korit.senicare.dto.request.auth.IdCheckRequestDto;
+import com.korit.senicare.dto.response.ResponseDto;
+import com.korit.senicare.service.AuthService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/id-check")
+    public ResponseEntity<ResponseDto> idCheck(
+        @RequestBody IdCheckRequestDto RequestBody
+    ) {
+        ResponseEntity<ResponseDto> response = authService.idCheck(RequestBody);
+        return response;
+    }
+}
